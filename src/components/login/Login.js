27ce41c65b9aa893c02ec4fe67e12b2b9fc58f6a
Atanas Tachev <81210808+AtanasTachev@users.noreply.github.com>
2022-02-23 00:09:13 +0200

    add google login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,10 +1,34 @@
-import React from "react";
-
 import './login.css'
+import React from "react";
+import { GoogleLogin } from 'react-google-login';
+const clientId = '415229235264-luri6la7g3qnf2ii89jj2limf6g52n9m.apps.googleusercontent.com';
+
 
 const LoginButton = () => {
+  const onSuccess = (res) => {
+    console.log('[Login Success] currentUser:', res.profileObj);
+  };
 
-  return <button className="login__button"><a href="https://accounts.google.com/servicelogin/signinchooser?flowName=GlifWebSignIn&flowEntry=ServiceLogin">LOGIN</a></button>;
+  const onFailure = (res) => {
+    console.log('[Login Failure] res:' , res);
+  } 
+
+  return (
+    <div className="login__button">
+      <GoogleLogin
+      clientId={clientId}
+      buttonText={'Login'}
+      onSuccess={onSuccess}
+      onFailure={onFailure}
+      cookiePolicy={'single_host_origin'}
+      isSignedIn={true}
+
+
+/>
+    </div>
+  )
+
+  // return <button className="login__button"><a href="https://accounts.google.com/servicelogin/signinchooser?flowName=GlifWebSignIn&flowEntry=ServiceLogin">LOGIN</a></button>;
 };
 
 export default LoginButton;
