9666967a1a0f104c425f369a9f89b3a0c8673036
Atanas Tachev <81210808+AtanasTachev@users.noreply.github.com>
2022-02-25 15:24:57 +0200

    implementing search onchange

--- src/components/cards/gallery/Gallery.js
+++ src/components/cards/gallery/Gallery.js
@@ -9,6 +9,7 @@ import * as cardServise from '../../../services/cardService';
 const Gallery = () => {
     const [firstHalf, setFirstHalf] = useState([]);
     const [secondHalf, setSecondHalf] = useState([]);
+    const [inputText, setInputText] = useState('');
     // const [user, setUser] = useState();
 
 
@@ -25,23 +26,55 @@ const Gallery = () => {
         })
     }, []);
 
- 
-
     const allAnimals = firstHalf.concat(secondHalf);
-    console.log(allAnimals);
+    
+    let filtered = [];
+    
+    let animalsText = allAnimals?.map(x => x.name.toLowerCase() + x.latin_name.toLowerCase() + x.animal_type.toLowerCase() + x.diet.toLowerCase());
+
+   console.log(animalsText);
+   let found = [];
+
+    const searchHandler = (e) => {
+        var lowerCase = e.target.value.toLowerCase();
+        setInputText(lowerCase);
+
+        found = animalsText.filter(x => {return x.search(inputText) != -1})
+        // const regex = new RegExp(inputText, 'g', 'i');
+        // found = animalsText.map(x => regex.exec(x));
+        // console.log(animalsText[0]);
+        // found.filter(x => x !== null);
+        // console.log(found);
+        // found = animalsText[0].filter(x => x.includes(inputText));
+        // filtered = result;
+        // found = animalsText.filter(x => x.includes(inputText))
+    }
+
+    console.log(inputText);
+    console.log(found);
+
+
+    // console.log(allAnimals);
     // console.log(user);
+    
+    // const searchFunction = () => {
+    // }
 
     return (
-        <>
+<>
         <header className='name'>
             <p className='user'>Name</p>
         </header>
         <div className='searchbar'>
-            <input className='input' placeholder='mon...'></input>
-            <button className='search'>search</button>
+            <input className='input' placeholder='mon...' onChange={searchHandler}></input>
+            {/* <button className='search' onClick={searchFunction}>search</button> */}
         </div>
+  
         <ul className='container'>
-            {allAnimals?.map(x => <SingleCard key={x.id} animal={x} />)}
+        {filtered.length>0
+         ? filtered?.map(x => <SingleCard key={x.id} animal={x} />)
+         : allAnimals?.map(x => <SingleCard key={x.id} animal={x} />)
+        }
         </ul>
         </>
     )
---
+++ src/components/search/Search.js
@@ -0,0 +1,27 @@
+// import './search.css';
+// import SingleCard from '../singleCard/SingleCard';
+// import { useEffect, useState } from 'react';
+
+// import * as cardServise from '../../../services/cardService';
+
+// const Search = (allAnimals) => {
+
+//     // let filteredAnimals = 
+
+// return (
+//     <>
+//     <header className='name'>
+//         <p className='user'>Name</p>
+//     </header>
+//     <div className='searchbar'>
+//         <input className='input' placeholder='mon...' onChange={searchHandler}></input>
+//         <button className='search' onClick={'/search'}>search</button>
+//     </div>
+//     <ul className='container'>
+//         {filteredAnimals?.map(x => <SingleCard key={x.id} animal={x} />)}
+//     </ul>
+//     </>
+// )
+// };
+
+// export default Search;
